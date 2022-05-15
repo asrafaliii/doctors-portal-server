20048d3194bfb4b1481f392cef26ccb8a69a5201
asrafaliii <asraf.bookaand@gmail.com>
2022-05-15 12:42:02 +0600

    find available time slots for a day

--- index.js
+++ index.js
@@ -27,6 +27,28 @@ async function run(){
             res.send(services);
         });
 
+        app.get('/available', async(req, res) =>{
+            const date = req.query.date || 'May 15, 2022';
+
+            // step 1: get all services
+            const services = await serviceCollection.find().toArray();
+
+            // step 2: get the booking of that day
+            const query = {date: date};
+            const bookings = await bookingCollection.find(query).toArray();
+
+            
+
+            // step 3: for each service, find booking for that service
+            services.forEach(service =>{
+                const serviceBookings = bookings.filter(book=> book.treatment  === service.name);
+                const booked = serviceBookings.map( s=> s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.available = available;
+            })
+            res.send(services);
+        })
+
         /**
          * API Naming Convention
          * app.get('/booking') // get all boking in this collection. or get more than one or by filter
@@ -39,8 +61,13 @@ async function run(){
         app.post('/booking', async(req, res) =>{
             booking = req.body;
             const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
+            const exists = await bookingCollection.findOne(query);
+            if(exists){
+                return res.send({success: false, booking: exists})
+            }
+
             const result = await bookingCollection.insertOne(booking);
-            res.send(result);
+            return res.send({success: true, result});
 
         })
 
